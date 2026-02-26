e35a0fb45b2738552113c5ccef161971852d1a8e
siddharthaSailsLearning <gcplearning.2025.sre@gmail.com>
2026-02-26 16:35:33 +0530

    my first commit modified

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'hobbies.app',
+  appName: 'hobbies-app',
+  webDir: 'dist'
+};
+
+export default config;
